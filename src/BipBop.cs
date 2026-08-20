52176b0c18641ad53965c2e29a266fb9db391936
Jonathan Schwalbe Öhrström <jonathan@ohrstrom.nu>
2026-08-20 13:31:09 +0200

    Continued work

---
+++ src/BipBop.cs
@@ -0,0 +1,21 @@
+﻿using McAdminPlugins;
+
+namespace BipBopPlugin;
+
+public class BipBop(IServerPluginFiles files, IPluginNavigation nav) : IPlugin
+{
+    public static IServerPluginFiles? Files { get; private set; }
+    
+    public Task Load()
+    {
+        Files = files;
+        
+        nav.AddPage(
+            text: "BipBop",
+            href: "bipbop",
+            administratorOnly: false
+        );
+        
+        return Task.CompletedTask;
+    }
+}
