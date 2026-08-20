08522833b97d330215a5f1a85af2085b9fa6af3d
Jonathan Schwalbe Öhrström <jonathan@ohrstrom.nu>
2026-08-20 16:48:14 +0200

    Update according to the new addon API

--- src/BipBop.cs
+++ src/BipBop.cs
@@ -2,7 +2,7 @@
 
 namespace BipBopPlugin;
 
-public class BipBop(IServerPluginFiles files, IPluginNavigation nav) : IPlugin
+public class BipBop(IServerPluginFiles files, IPluginPages pages) : IPlugin
 {
     public static IServerPluginFiles? Files { get; private set; }
     
@@ -10,11 +10,65 @@ public class BipBop(IServerPluginFiles files, IPluginNavigation nav) : IPlugin
     {
         Files = files;
         
-        nav.AddPage(
-            text: "BipBop",
-            href: "bipbop",
-            administratorOnly: false
-        );
+        pages.AddPage(new PluginPage("bipbop-settings", "BipBop")
+        {
+            AdministratorOnly = false,
+            Description = "Settings for BipBop",
+            Sections = [
+                new PluginSettingsSection
+                {
+                    Title = "Settings",
+                    Description = "Settings for the plugin",
+                    LoadAsync = async ct => [ // TODO during load set current value from file
+                        // Celebrations
+                        new PluginField("celebration.chicken", "Chicken")
+                        {
+                            Description = "Spawn celebratory chicken on achievements",
+                            Kind = PluginFieldKind.Toggle,
+                            Value = "true",
+                            Group = "Celebrations"
+                        },
+                        new PluginField("celebration.chicken_name", "Chicken Name")
+                        {
+                            Description = "Format the name of the chicken. Leave empty to give it no name at all.\n'%' serves as a placeholder for the player name",
+                            Kind = PluginFieldKind.Text,
+                            Value = "[PLAYER]'s partykyckling",
+                            Group = "Celebrations"
+                        },
+                        new PluginField("celebration.fireworks", "Fireworks")
+                        {
+                            Description = "Spawn fireworks on achievements",
+                            Kind = PluginFieldKind.Toggle,
+                            Value = "true",
+                            Group = "Celebrations"
+                        },
+                        new PluginField("celebration.fireworks_flight", "Fireworks flight time")
+                        {
+                            Description = "How long should the fireworks flight time be?",
+                            Kind = PluginFieldKind.Number,
+                            Value = "0",
+                            Minimum = 0,
+                            Maximum = 2,
+                            Group = "Celebrations"
+                        },
+                        
+                        // Portals
+                        new PluginField("portals.wand_required", "Require wand")
+                        {
+                            Description = "Is the user required to craft a wand in order to set portals?",
+                            Kind = PluginFieldKind.Toggle,
+                            Value = "false",
+                            Group = "Portals"
+                        }
+                    ],
+                    SaveAsync = async (changes, ct) =>
+                    {
+                        // TODO write to correct files
+                        return PluginResult.Success("Successfully saved!");
+                    }
+                }
+            ]
+        });
         
         return Task.CompletedTask;
     }
